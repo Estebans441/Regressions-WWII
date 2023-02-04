294df3cfc10a916c8a82d2dc25c376d819f5a726
Estebans441 <estebans441@gmail.com>
2023-02-03 20:30:05 -0500

    polynomial-regression-implementation

--- polynomial_regression/polynomial_regression.py
+++ polynomial_regression/polynomial_regression.py
@@ -1,37 +1,45 @@
+# Multiple Linear Regression
+
 # Importing the libraries
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import r2_score
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
+from sklearn import linear_model
 
 # Importing the dataset
 dataset = pd.read_csv('../datasets/Summary of Weather.csv', low_memory= False)
-x1 = dataset.iloc[:, 5:6].values  # MinTemp
-x2 = dataset.iloc[:, 6:7].values  # MeanTemp
-x3 = dataset.iloc[:, 2:3].values  # Precip
-y = dataset.iloc[:, 4:5].values   # MaxTemp
-x3[x3 == ['T']] = 0
-x3 = x3.astype(np.float64)
+X = dataset.iloc[:, [0, 5, 6]].values   # Station, MinTemp, MeanTemp
+y = dataset.iloc[:, [4]].values         # MaxTemp
+
+# Preprocessing
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaler.fit(X)
+X = scaler.transform(X)
+
+np.set_printoptions(precision=2)
+print(X)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(np.column_stack((x1,x2,x3)), y, test_size=0.33, random_state=0)
 
-# Fitting Polynomial Regression to the dataset
-poly_reg = PolynomialFeatures(degree=3)
-X_poly = poly_reg.fit_transform(x_train)
-poly_reg.fit(X_poly, y_train)
-lin_reg = LinearRegression()
-lin_reg.fit(X_poly, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/3, random_state=0)
 
-# Predicting the Test set results
-X_test_poly = poly_reg.transform(x_test)
-y_pred = lin_reg.predict(X_test_poly)
+# 5 degree polynomial features
+deg_of_poly = 3
+poly = PolynomialFeatures(degree=deg_of_poly)
+X_ = poly.fit_transform(X_train)
+
+# Fit linear model
+clf = linear_model.LinearRegression()
+clf.fit(X_, y_train)
+
+X_2 = poly.fit_transform(X_test)
+y_pred = clf.predict(X_2)
 
 # Evaluating the Model Performance
-from sklearn.metrics import mean_squared_error, r2_score
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 r2 = r2_score(y_test, y_pred)
-print("Root Mean Squared Error: ", rmse)
 print("R^2 Score: ", r2)
+print("Coefficients: ", clf.coef_)
