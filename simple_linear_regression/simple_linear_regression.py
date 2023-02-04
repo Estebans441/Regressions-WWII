571b0c7ab6f8fcd6a7ea17ae19f5ae9d576eaca8
Estebans441 <estebans441@gmail.com>
2023-02-03 19:20:21 -0500

    fix-datasets-preprocessing

--- simple_linear_regression/simple_linear_regression.py
+++ simple_linear_regression/simple_linear_regression.py
@@ -13,13 +13,17 @@ x1 = dataset.iloc[:, [5]].values  # MinTemp
 x2 = dataset.iloc[:, [6]].values  # MeanTemp
 x3 = dataset.iloc[:, [2]].values  # Precip
 y = dataset.iloc[:, [4]].values   # MaxTemp
-x3[x3 == ['T']] = 0
-x3 = x3.astype(np.float64)
+
+# Preprocessing de data of 3rd variable deleting rows equal to T(unknown data)
+indexes = np.where(x3[:] == ['T'])
+x3 = np.delete(x3, indexes, axis=0).astype(np.float64)
+y2 = np.delete(y, indexes, axis=0)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 
-X1_train, X1_test, X2_train, X2_test, X3_train, X3_test, y_train, y_test = train_test_split(x1, x2, x3, y, test_size=1/3, random_state=0)
+X1_train, X1_test, X2_train, X2_test, y_train, y_test = train_test_split(x1, x2, y, test_size=1/3, random_state=0)
+X3_train, X3_test, y2_train, y2_test = train_test_split(x3, y2, test_size=1/3, random_state=0)
 
 """
 Var 1 - MinTemp
@@ -31,6 +35,10 @@ regressor.fit(X1_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X1_test)
 
+# Evaluating the Model Performance
+r2 = r2_score(y_test, y_pred)
+print("MeanTemp R^2 Score: ", r2)
+
 # Visualising the Training set results
 plt.scatter(X1_train, y_train, color='lightcoral')
 plt.scatter(X1_test, y_test, color='red')
@@ -39,10 +47,6 @@ plt.title('MaxTemp vs MinTemp (Test set)')
 plt.xlabel('MinTemp')
 plt.ylabel('MaxTemp')
 plt.show()
-
-# Evaluating the Model Performance
-r2 = r2_score(y_test, y_pred)
-print("MinTemp R^2 Score: ", r2)
 
 """
 Var 2 - MeanTemp
@@ -54,6 +58,10 @@ regressor.fit(X2_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X2_test)
 
+# Evaluating the Model Performance
+r2 = r2_score(y_test, y_pred)
+print("MeanTemp R^2 Score: ", r2)
+
 # Visualising the Training set results
 plt.scatter(X2_train, y_train, color='limegreen')
 plt.scatter(X2_test, y_test, color='green')
@@ -63,29 +71,25 @@ plt.xlabel('MeanTemp')
 plt.ylabel('MaxTemp')
 plt.show()
 
-# Evaluating the Model Performance
-r2 = r2_score(y_test, y_pred)
-print("MeanTemp R^2 Score: ", r2)
-
 """
 Var 3 - Precip
 """
 # Training the Simple Linear Regression model on the Training set
 regressor = LinearRegression()
-regressor.fit(X3_train, y_train)
+regressor.fit(X3_train, y2_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X3_test)
 
+# Evaluating the Model Performance
+r2 = r2_score(y2_test, y_pred)
+print("Precip R^2 Score: ", r2)
+
 # Visualising the Training set results
-plt.scatter(X3_train, y_train, color='navajowhite')
-plt.scatter(X3_test, y_test, color='darkorange')
+plt.scatter(X3_train, y2_train, color='navajowhite')
+plt.scatter(X3_test, y2_test, color='darkorange')
 plt.plot(X3_train, regressor.predict(X3_train), color='blue')
 plt.title('MaxTemp vs Precip (Test set)')
 plt.xlabel('Precip')
 plt.ylabel('MaxTemp')
 plt.show()
-
-# Evaluating the Model Performance
-r2 = r2_score(y_test, y_pred)
-print("Precip R^2 Score: ", r2)
